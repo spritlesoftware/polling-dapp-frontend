123bbe4fd1723e5280a02acb65ae9100dde2ac0e
Ramya1520 <ramya.krishnan1520@gmail.com>
2023-01-20 11:06:13 +0530

    missed to stage in prev commit

--- src/Listcontext/listcontext.tsx
+++ src/Listcontext/listcontext.tsx
@@ -9,18 +9,18 @@ import RPC from "../web3RPC"
 import "../App.css";
 const clientId = "BO_fSxnxQUgm7OW9FgRGzU2ID0PPDAfFLgftNxsjFZkDgS-KwasdSt8opMKjB1eY6ouoDvHtv2gl1u7xrlBeksc";
 
-export const PollingContext = createContext<{
+export const PollingContext = createContext<{poll_question: {
     ques: string;
     option1: string;
     option2: string;
     date: string;
     vote: number;
-} []>([])
+}[], userDetails: {username:string;usermail:string;rpc:any}}>({poll_question: [], userDetails: {username: '', usermail: '', rpc:{}}})
 
 const ListProvider = ({children}: React.PropsWithChildren) => {
     const [userDetails,setUserDetails]=useState({  username: "", usermail: "", rpc: {}})
     const poll_question = [{ ques: "Are you ok?",option1:"Yes",option2:"No",date:"22-mar-2022",vote:87},{ ques: "Do you know spritle birthday?",date:"22-jan-2023",option1:"Yes",option2:"No",vote:46},{ques: " Do you have middle name?",date:"22-mar-2022",vote:8,option1:"Yes",option2:"No"}]
-    return <PollingContext.Provider value={poll_question}>
+    return <PollingContext.Provider value={{poll_question: poll_question, userDetails: userDetails}}>
         {children}
     </PollingContext.Provider>
 }
