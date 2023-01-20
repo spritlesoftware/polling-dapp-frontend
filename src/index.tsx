4aee5967a347ccb56bce433520c37c79310ff011
Ramya1520 <ramya.krishnan1520@gmail.com>
2023-01-20 20:48:36 +0530

    missed from previous commit

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import '../node_modules/bootstrap/dist/css/boostrap.css'
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../node_modules/@fortawesome/fontawesome-free/css/all.css'
 import { BrowserRouter } from 'react-router-dom';
@@ -21,23 +20,15 @@ root.render(
   <React.StrictMode>
     <ListProvider>
     <BrowserRouter>
-   {/* <NavBar> */}
    <div className='index'>
     <Routes>
       <Route path='/pole' element={<Polling/>}></Route>
       <Route path='' element={<App/>}></Route>  
       <Route path='/thankyou' element={<Thankpage/>}></Route>
-      {/* <Route path='/card' element={<Card  />}></Route> */}
-      {/* <Route path='/card'element={<Card/>}></Route> */}
     </Routes>
     </div>
-    {/* </NavBar> */}
     </BrowserRouter>
     </ListProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
